1d9d3282b1fc8adffb1c50d90d800e60f59aa9ca
hedi <hedihamida98@gmail.com>
2022-11-30 00:47:48 +0100

    dbstorage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sqlalchemy import *
+
+class DBStorage:
+    """DB storage engine"""
+    __engine = None
+    __session = None
+    user = "hbnb_dev"
+    password = "hbnb_dev_pwd"
+    host = '127.0.0.1'
+    database = 'hbnb_dev_db'
+    def __init__(self):
+        self.__engine = create_engine(f"mysql+pymysql://{self.user}:{self.password}@localhost/{self.database}",
+        connect_args = dict(host='localhost'),pool_pre_ping=True)
+    
+    def close(self):
+        self.__session.remove()    
