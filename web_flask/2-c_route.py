9c092da9a68388fbba48ce7b312bfe96701d4b84
hedi <hedihamida98@gmail.com>
2022-11-29 22:42:03 +0100

    /c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+"""/route"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_isfun(text):
+    text = text.replace("_", " ")
+    return f"C{text}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
