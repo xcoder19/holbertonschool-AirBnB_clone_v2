09829f40c5fee9893a8e0b9220ced967d066fa34
hedi <hedihamida98@gmail.com>
2022-11-29 23:03:17 +0100

    documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_isfun(text):
-    return "C{}".format(text.replace("_"," "))
+    return "C{}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
