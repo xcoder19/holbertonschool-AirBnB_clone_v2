039d2e4cfb74600bad9aa94d8ffc059d289d7b3f
hedi <hedihamida98@gmail.com>
2022-11-29 20:57:15 +0100

    flask server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""flask app"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +8,5 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
-if __name__:
+if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
