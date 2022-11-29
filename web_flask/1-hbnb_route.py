a7025624e1db4c2b4b814f82cbe5028bfb778b04
hedi <hedihamida98@gmail.com>
2022-11-29 22:33:10 +0100

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
-@app.route("/hbnb",strict_slashes=False)
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
