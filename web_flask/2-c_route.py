d083aa6421646c0609d51a7d81f90c6b7f02798d
hedi <hedihamida98@gmail.com>
2022-11-29 23:00:03 +0100

    documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,42 +1,29 @@
 #!/usr/bin/python3
-"""flask app"""
+"""
+flask app
 
+"""
 
 from flask import Flask
 
-""" app instance """
 app = Flask(__name__)
-
-"""home route"""
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """home route"""
 
     return "Hello HBNB!"
 
 
-"""hbnb route"""
-
-
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """hbnb route"""
     return "HBNB"
-
-
-"""c_is fun route """
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_isfun(text):
-    """c_is fun route """
     text = text.replace("_", " ")
     return f"C{text}"
-
-
-"""host and port"""
 
 
 if __name__ == '__main__':
