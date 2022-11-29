c58f7266b8a10d592bf68d88dbee1f992ddaff24
hedi <hedihamida98@gmail.com>
2022-11-29 22:52:13 +0100

    /c route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,28 +4,39 @@
 
 from flask import Flask
 
-
+""" app instance """
 app = Flask(__name__)
+
+"""/ route"""
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """/ route"""
+
     return "Hello HBNB!"
+
+
+"""/hbnb route"""
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """/hbnb route"""
     return "HBNB"
-
 
 
 """c_is func route """
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_isfun(text):
+    """c_is func route """
     text = text.replace("_", " ")
     return f"C{text}"
 
+
+"""host and port"""
 
 
 if __name__ == '__main__':
