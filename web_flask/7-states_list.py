fa6a1ffec90d008be4d70f2b40ae43b1e7c6af60
hedi <hedihamida98@gmail.com>
2022-11-30 01:11:04 +0100

    states route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """flask app"""
-
-
 from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """return states"""
     return render_template("7-states_list.html", data=storage.all(State))
 
 
 @app.teardown_appcontext
 def teardown():
+    """close storage"""
     storage.close()
 
 
