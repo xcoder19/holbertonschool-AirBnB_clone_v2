f889d96944a56b29964daf38cece415459c9db2a
hedi <hedihamida98@gmail.com>
2022-12-02 14:08:58 +0100

    revert

--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import *
+
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128),nullable = False)
+    name = ""
