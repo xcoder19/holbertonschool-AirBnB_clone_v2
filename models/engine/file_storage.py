6d2ed93ece8b4a5283f66df11d52585db74e4034
hedi <hedihamida98@gmail.com>
2022-12-02 14:01:49 +0100

    revert

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,17 +10,7 @@ class FileStorage:
 
     def all(self,cls=None):
         """Returns a dictionary of models currently in storage"""
-        newdict = {}
-        if cls is not None:
-            for x,i in self.__objects.items():
-                if cls == i.__class__:
-                    newdict.update({x:i})
-            return newdict
-
         return FileStorage.__objects
-                
-        
-        
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -61,12 +51,11 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete obj from __objects"""
-        if obj is None or len(self.__objects) == 0:
+        if obj is None:
             return
         self.__objects.pop(f"{obj.to_dict()['__class__'] + '.' + obj.id}")
         
-    def close(self):
-        self.reload()
+        
 
 
 
