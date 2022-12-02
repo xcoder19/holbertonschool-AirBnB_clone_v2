22b0dc85a766a9191e10c5681dbcb3c041078593
hedi <hedihamida98@gmail.com>
2022-12-02 14:05:49 +0100

    revert

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,24 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import *
+
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60),unique=True,nullable=False,primary_key=True)
-    #created_at = Column(datetime,nullable=False,default = datetime.utcnow())
-    #updated_at = Column(datetime,nullable=False,default= datetime.utcnow())
-
     def __init__(self, *args, **kwargs):
-            """Instatntiates a new model"""
-            
-                                       
-           
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -26,7 +31,6 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -37,10 +41,4 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if (dictionary['_sa_instance_state']):
-            dictionary.pop('_sa_instance_state')
         return dictionary
-    
-    def delete(self):
-        from models import storage
-        storage.delete(self)
