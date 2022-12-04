d009b0df76dee70b9309b7903d708e158849498d
hedi <hedihamida98@gmail.com>
2022-12-04 20:38:11 +0100

    pep8

--- models/state.py
+++ models/state.py
@@ -12,18 +12,19 @@ from os import getenv
 if getenv("HBNB_TYPE_STORAGE") == "db":
     class State(BaseModel, Base):
         """ State class """
-        
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all, delete')
 else:
     class State(BaseModel):
         name = ""
+
         @property
         def cities(self):
             cities_state_relationship = []
             cities = models.storage.all(City)
-            for k,v in cities:
+            for k, v in cities:
                 if v.state_id == self.id:
                     cities_state_relationship.append(v)
             return cities_state_relationship
