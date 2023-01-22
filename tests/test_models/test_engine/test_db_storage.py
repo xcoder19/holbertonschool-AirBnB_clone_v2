cdaf27fe39983b596c588f9ab057a31942b545a5
hedi <hedihamida98@gmail.com>
2023-01-22 20:48:36 +0100

    test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+testing db_storage
+"""
+import unittest
+
+
+class test_dbstorage(unittest.TestCase):
+    """test dbstorage"""
+    def test_dbstorage(self):
+        """test dbstorage"""
+        self.assertEqual(1, 1)
